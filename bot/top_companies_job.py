5c394f01b4a6cecd4317e21f92027b02338aa3ee
J-Obog <jobogbaimhe@gmail.com>
2023-11-19 18:40:49 -0500

    polishing up job

--- bot/top_companies_job.py
+++ bot/top_companies_job.py
@@ -3,17 +3,18 @@ import requests
 import csv
 
 OUTPUT_PATH = "companies.csv"
+URL = "https://stockanalysis.com/list/biggest-companies/"
     
 with open(OUTPUT_PATH, 'w+', newline='') as csvfile:
     fieldnames = ["ticker", "company", "mkt_cap"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     
-    html_content = requests.get("https://stockanalysis.com/list/biggest-companies/").content
+    html_content = requests.get(URL).content
     sp = BeautifulSoup(html_content, "html.parser")
     tbl = sp.find(id="main-table")
-    tbody = tbl.find("tbody").find_all("tr")
+    tbody = tbl.find("tbody")
 
-    for r in tbody:
-        tds = r.find_all("td")        
+    for tr in tbody.find_all("tr"):
+        tds = tr.find_all("td")        
         writer.writerow({'ticker': tds[1].text, "company": tds[2].text, "mkt_cap": tds[3].text})
