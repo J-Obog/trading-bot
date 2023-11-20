bf60004ddbbcc6f3f386a9aafa84b8d097fc8695
J-Obog <jobogbaimhe@gmail.com>
2023-11-19 19:22:33 -0500

    adding client method to get holdings

--- trading/data.py
+++ trading/data.py
@@ -4,4 +4,33 @@ from dataclasses import dataclass
 class Order:
     quantity: int
     symbol: str
-    transaction_type: str
+    transaction_type: str # should be an enum
+
+"""
+{
+    "stock": {
+        "symbol": "BRK.A",
+        "description": "Berkshire Hathaway Inc. - Ordinary Shares - Class A",
+        "technical": {
+            "lastPrice": 544190,
+            "__typename": "Technical"
+        },
+        "__typename": "Stock"
+    },
+    "symbol": "BRK.A",
+    "quantity": 199304286,
+    "purchasePrice": 522669.58,
+    "marketValue": 108459399398340,
+    "dayGainDollar": 0,
+    "dayGainPercent": 0,
+    "totalGainDollar": 4289112402833.55,
+    "totalGainPercent": 4.12,
+    "__typename": "ExecutedStockHolding"
+}
+"""
+
+@dataclass
+class Holding:
+    symbol: str
+    quantity: int
+    holding_type: str # should be enum
--- trading/investopedia.py
+++ trading/investopedia.py
@@ -1,6 +1,9 @@
+from typing import List
 import requests
 
-from trading.data import Order
+from trading.data import Holding, Order
+
+API_URL = "https://api.investopedia.com/simulator/graphql"
 
 class InvestopediaClient:
     def __init__(self):
@@ -21,3 +24,39 @@ class InvestopediaClient:
             },
             "query": "mutation StockTrade($input: TradeEntityInput!) { submitStockTrade(stockTradeEntityInput: $input) { ... on TradeInvalidEntity { errorMessages __typename } ... on TradeInvalidTransaction { errorMessages __typename } __typename } }"
         }
+
+        self.sess.post(API_URL, json=body)
+
+    def get_holdings(self) -> List[Holding]:
+        holdings = []
+
+        for htype in ["STOCKS", "SHORTS"]:
+            holdings.extend(
+                self._get_holdings(htype)
+            )
+
+        return holdings
+
+    def _get_holdings(self, holding_type: str) -> List[Holding]:
+        holdings = [] 
+
+        body = {
+            "operationName": "StockHoldings",
+            "variables": {
+                "holdingType": holding_type,
+            },
+            "query": "query StockHoldings($portfolioId: String!, $holdingType: HoldingType!) { readPortfolio(portfolioId: $portfolioId) { ... on PortfolioErrorResponse { errorMessages __typename } ... on Portfolio { holdings(type: $holdingType) { ... on HoldingsErrorResponse { errorMessages __typename } ... on CategorizedHoldingsErrorResponse { errorMessages __typename } ... on CategorizedStockHoldings { holdingsSummary { marketValue dayGainDollar dayGainPercent totalGainDollar totalGainPercent __typename } executedTrades { stock { ... on Stock { symbol description technical { lastPrice __typename } __typename } __typename } symbol quantity purchasePrice marketValue dayGainDollar dayGainPercent totalGainDollar totalGainPercent __typename } __typename } __typename } __typename } __typename } }"
+            }
+        
+        data = self.sess.post(API_URL, json=body).json()["data"]["readPortfolio"]["holdings"]["executedTrades"]
+
+        for holding in data:
+            holdings.append(
+                Holding(
+                    symbol=holding["symbol"],
+                    quantity=holding["quantity"],
+                    holding_type=holding_type
+                )
+            )
+        
+        return holdings
