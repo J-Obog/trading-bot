6cea9ff7d949b02208e1e9b4801ca8e92829f775
J-Obog <jobogbaimhe@gmail.com>
2023-11-23 02:46:32 -0500

    adding spreadsheet sync task

--- spreadsheet_sync_task.sh
+++ spreadsheet_sync_task.sh
@@ -5,4 +5,4 @@ apt-get install -y python3-pip
 python3 -m pip install --upgrade pip
 python3 -m pip install pipenv
 python3 -m pipenv install
-python3 -m pipenv run python3 trade.py
+python3 -m pipenv run python3 sync.py
