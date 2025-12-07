0e8af2604a1e287e49c85c0d11e2dfcbfdba1b26
J-Obog <jobogbaimhe@gmail.com>
2025-12-06 22:39:09 -0500

    starting new

--- paper_trading_task.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install -y python3-pip
-python3 -m pip install --upgrade pip
-python3 -m pip install pipenv
-python3 -m pipenv install
-python3 -m pipenv run python3 trade.py
-python3 -m pipenv run python3 sync.py
