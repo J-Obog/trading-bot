e839e44a64ed2d178c3657a195235422da6e9c9f
J-Obog <jobogbaimhe@gmail.com>
2025-12-16 08:14:59 -0500

    with market cap

--- ticker_file_builder.py
+++ ticker_file_builder.py
@@ -1,17 +1,20 @@
 import json
 import csv
 
-tickers = set()
+tickers = []
 
 def populate_tickers_from_file(filename):
     with open(filename, mode='r', encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
 
         for row in csv_reader:
-            tickers.add(row["Symbol"])
+            tickers.append({
+                "Symbol": row["Symbol"], 
+                "Market Cap": float(row["Market Cap"].replace("$", ""))
+            })
 
 populate_tickers_from_file("nyse.csv")
 populate_tickers_from_file("nasdaq.csv")
 
 with open("tickers.json", "w+", encoding="utf-8") as json_file:
-    json.dump(sorted(list(tickers)), json_file)
+    json.dump(sorted(list(tickers), key=lambda x: x["Symbol"]), json_file)
